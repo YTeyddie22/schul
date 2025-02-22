3b3c1358e72221820a3a57e2e7c1b4892a9518a8
YTeyddie22 <teyddie@gmail.com>
2025-02-22 08:57:17 +0300

    feat(page): implement lessons page

---
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+
+import { lessonsData, role } from "@/lib/data";
+import Link from "next/link";
+
+const columns = [
+    {
+        header: "Subject Name",
+        accessor: "name",
+    },
+    {
+        header: "Class",
+        accessor: "class",
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Teacher",
+        accessor: "teacher",
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Actions",
+        accessor: "action",
+    },
+];
+
+type LesssonsList = {
+    id: number;
+    subject: string;
+    class: string;
+    teacher: string;
+};
+
+const LesssonsListPage = () => {
+    const renderRow = (items: LesssonsList) => (
+        <tr
+            key={items.id}
+            className="border-b border-gray-300 even:bg-slate-50 text-sm hover:bg-tedSkyLight">
+            <td className="flex items-center gap-4 p-4">{items.subject}</td>
+            <td className="hidden md:table-cell">{items.class}</td>
+            <td className="hidden md:table-cell">{items.teacher}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teachers/${items.id}`}>
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-tedSkyLight">
+                            <Image
+                                src="/edit.png"
+                                alt="edit"
+                                width={16}
+                                height={16}
+                            />
+                        </button>
+                    </Link>
+                    {role === "admin" && (
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-tedPurpleLight">
+                            <Image
+                                src="/delete.png"
+                                alt="delete"
+                                width={16}
+                                height={16}
+                            />
+                        </button>
+                    )}
+                </div>
+            </td>
+        </tr>
+    );
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            <div className="flex items-center justify-between">
+                <h1 className="hidden md:block text-lg font-semibold">
+                    All Lessons
+                </h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <TableSearch />
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-tedYellow">
+                            <Image
+                                src="/filter.png"
+                                alt="filter"
+                                width={14}
+                                height={14}
+                            />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-tedYellow">
+                            <Image
+                                src="/sort.png"
+                                alt="sort"
+                                width={14}
+                                height={14}
+                            />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-tedYellow">
+                            <Image
+                                src="/plus.png"
+                                alt="plus"
+                                width={14}
+                                height={14}
+                            />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            {/**List */}
+            <Table columns={columns} renderRow={renderRow} data={lessonsData} />
+
+            {/**Pagination */}
+            <Pagination />
+        </div>
+    );
+};
+
+export default LesssonsListPage;
